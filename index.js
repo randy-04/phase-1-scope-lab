7d91c71540e19587fc8ab599179f3a4f7e0f2be6
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2022-11-24 13:47:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    uc = customerName.toUpperCase();
+    return uc;
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "him";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "not him";
+}
+
+console.log(leastFavoriteCustomer);
